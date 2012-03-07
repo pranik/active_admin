4698af40a3721f7cf45657c9238d2ea356420910
Nicola Prando <nicola.prando@intesys.it>
2012-03-07 11:05:01 +0100

    replaced exists? in items_in_collection? with .count == 1. this fix ThrowResult with empty result on search.

--- lib/active_admin/views/pages/index.rb
+++ lib/active_admin/views/pages/index.rb
@@ -40,7 +40,7 @@ module ActiveAdmin
           #
           # If we don't reorder, there may be some columns referenced in the order
           # clause that requires the original select.
-          collection.reorder("").limit(1).exists?
+          collection.reorder("").limit(1).count == 1
         end
 
         def build_collection
